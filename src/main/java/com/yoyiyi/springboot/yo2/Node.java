8b1f96f56d27e3be0b0f5f571225762b5ee3623f
zzq <2455676683@qq.com>
2017-12-18 18:28:05 +0800

    yoyiyi

---
+++ src/main/java/com/yoyiyi/springboot/yo2/Node.java
@@ -0,0 +1,26 @@
+package com.yoyiyi.springboot.yo2;
+
+/**
+ * 类名：Node
+ * 描述：TODO
+ * 时间：2017/12/18 18:23
+ *
+ * @author <a href="2455676683@qq.com">zzq</a>
+ */
+public class Node {
+    private void node() {
+        /**
+         *1.@{}  引用静态资源
+         *2.${}  访问属性
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         */
+
+    }
+}
